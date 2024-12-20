2a76f78cc8515aa4e5a0a79e94292e8f16005ae5
Akash Dabhane <akashdabhane10@gmail.com>
2024-12-21 00:12:05 +0530

    ProtectedRoute and authentication content is created

---
+++ src/utils/ProtectedRoute.jsx
@@ -0,0 +1,39 @@
+import { Navigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { baseUrl } from "../utils/helper";
+import { useAuth } from "../contexts/AuthContext";
+
+const ProtectedRoute = ({ children }) => {
+    const { loggedInUser, setLoggedInUser } = useAuth();
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get(`${baseUrl}/users/logged-in-user`,
+            {
+                withCredentials: true,
+                headers: {
+                    'Authorization': `Bearer ${Cookies.get('accessToken')}`,
+                }
+            })
+            .then((response) => {
+                setLoggedInUser(response.data.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            })
+    }, [])
+
+    // Show a loading spinner or nothing while fetching user data
+    if (isLoading) {
+        return <div className="text-center font-bold text-white text-4xl p-10">Loading...</div>; // Or a proper spinner
+    }
+
+    return loggedInUser !== null ? children : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
